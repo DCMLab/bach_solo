292acdfadf9700d706d5a2c9f38c9f1f74f589c2
Johannes Hentschel <johannes.hentschel@epfl.ch>
2024-09-14 16:52:47 +0200

    annotation workflow v4.7

--- .github/workflows/helper.py
+++ .github/workflows/helper.py
@@ -1,7 +1,9 @@
 import argparse
 import re
 import os
-def create_new_tag(tag):
+from datetime import datetime
+
+def create_new_tag(tag, update_major):
     if not (re.match(r'^v\d+\.\d+$', tag)):
         raise Exception(f'tag: {tag} is not giving in the correct format e.i v0.0')
     
@@ -11,20 +13,30 @@ def create_new_tag(tag):
     if len(digits_tags) != 2:
         raise Exception(f'tag: {tag} must contain two version digits')
     
-    new_digit = int(digits_tags[1]) + 1
-    return "v" + str(digits_tags[0]) + "." + str(new_digit)
+    major_num = int(digits_tags[0])
+    minor_num = int(digits_tags[1])
+    if update_major:
+        print(f"Label detected to update major version")
+        major_num += 1
+        minor_num = 0
+    else:
+        minor_num += 1
+    return f"v{major_num}.{minor_num}"
 
 def store_tag(tag):
     with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
         print(f'new_tag={tag}', file=fh)
 
-def update_file_with_tag(f_name, old_tag, new_tag):
+def update_file_with_tag(f_name, old_tag, new_tag, replace_dates=True):
     if os.path.isfile(f_name):
         try:
             with open(f_name, "r",encoding="utf-8") as f:
                 data = f.read()
             data = data.replace(old_tag, new_tag)
-
+            if replace_dates:
+                date_re = r"\d{4}-\d{2}-\d{2}"
+                today = datetime.today().strftime('%Y-%m-%d')
+                data = re.sub(date_re, today, data)
             with open(f_name, "w",encoding="utf-8") as f:
                 f.write(data)
         except Exception as e:
@@ -38,22 +50,39 @@ def main(args):
     if not tag:
         print(f"Warning: a latest release with a tag does not exist in current repository, starting from {new_tag}")
     else:
-        new_tag = create_new_tag(tag)
+        new_tag = create_new_tag(tag,args.update_major_ver)
         print(f"Repository with tag: {tag}, creating a new tag with: {new_tag}")
         update_file_with_tag(".zenodo.json", tag, new_tag)
         update_file_with_tag("CITATION.cff", tag, new_tag)
-        update_file_with_tag("README.md", tag, new_tag)
+        update_file_with_tag("README.md", tag, new_tag, replace_dates=False)
     store_tag(new_tag)
 
 def run():
     args = parser.parse_args()
     main(args)
 
+
+def str_to_bool(value):
+    if value.lower() == "true":
+        return True
+    elif value.lower() == "false":
+        return False
+    else:
+        raise Exception(
+            f"Error: value {value} as argument is not accepted\n"
+            f"retry with true or false"
+        )
+    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--tag", type=str,
         help="Require: latest tag",
+        required=True
+    )
+    parser.add_argument(
+        "--update_major_ver", type=str_to_bool,
+        help="Require: boolean to update the major tag number",
         required=True
     )
     run()
